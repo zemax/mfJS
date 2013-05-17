46d93f3f8f49aca97c56a2b8f74b26e3e9aaee86
zemax <max@zemax.net>
2013-05-17 10:38:35 +0200

    Inital Commit

---
+++ build/js/mf/animation/Sequencer.js
@@ -0,0 +1,89 @@
+(function(mf){
+	mf.animation = mf.animation || {};
+	
+	/**
+	 * Sequencer
+	 */
+	function Sequencer() {
+		this.length = 0;
+		this.animations = [];
+	}
+	
+	mf.animation.Sequencer = Sequencer;
+	
+	var p = Sequencer.prototype;
+	
+	/**
+	 * Register an animation object
+	 * 
+	 * @param animation
+	 * - start : start time : default : current end time
+	 * - duration : length of animation segment
+	 * - animate : function called during animation
+	 * - animateIn : function called once when entering the animation timeline
+	 * - animateOut : function called once when leaving the animation timeline
+	 * 
+	 * @returns
+	 */
+	p.register = function(animation) {
+		animation = $.extend({
+			start: this.length,
+			duration: 100,
+			animate: undefined,
+			animateIn: undefined,
+			animateOut: undefined,
+			started: false
+		}, animation);
+		
+		this.animations.push(animation);
+		
+		this.length = Math.max(this.length, animation.start + animation.duration);
+		
+		return animation;
+	};
+	
+	/**
+	 * Process animations
+	 */
+	p.process = function(t) {
+		var dt, animation;
+		
+		// Process Animate Out
+		for(var i=0, l=this.animations.length; i<l; i++) {
+			animation = this.animations[i];
+			dt = t - animation.start;
+			
+			if (animation.started && ((dt < 0) || (dt >= animation.duration))) {
+				if (typeof animation.animateOut == "function") {
+					animation.animateOut(dt, animation.duration);
+				}
+				animation.started = false;
+			}
+		}
+		
+		// Process Animate In
+		for(var i=0, l=this.animations.length; i<l; i++) {
+			animation = this.animations[i];
+			dt = t - animation.start;
+			
+			if (!animation.started && (dt >= 0) && (dt < animation.duration)) {
+				if (typeof animation.animateIn == "function") {
+					animation.animateIn(dt, animation.duration);
+				}
+				animation.started = true;
+			}
+		}
+		
+		// Process Animate
+		for(var i=0, l=this.animations.length; i<l; i++) {
+			animation = this.animations[i];
+			dt = t - animation.start;
+			
+			if ((dt >= 0) && (dt < animation.duration)) {
+				if (typeof animation.animate == "function") {
+					animation.animate(dt, animation.duration);
+				}
+			}
+		}
+	};
+})(mf = window.mf || {});
---
+++ build/js/mf/animation/TimeLine.js
@@ -0,0 +1,121 @@
+(function(mf){
+	mf.animation = mf.animation || {};
+	
+	/**
+	 * TimeLine
+	 * 
+	 * @param options :
+	 * - length (in milli-seconds)
+	 */
+	function TimeLine(options) {
+		this.options = $.extend({
+			length: 1
+		}, options);
+		
+		this.time 	= 0;
+		this.length = this.options.length;
+		
+		this.easing = {
+			easingFunction: mf.utils.Maths.easeOutQuad, // Easing function
+			startTime: 0,  // Start time
+			duration: 0,   // Easing duration
+			startValue: 0, // Start value to ease from
+			endValue: 0,   // End value to ease to
+			
+			on: false
+		};
+	}
+	
+	mf.animation.TimeLine = TimeLine;
+	
+	TimeLine.TIME_CHANGED 	= "eventTimeChanged";
+	TimeLine.PLAY 			= "eventPlay";
+	TimeLine.STOP 			= "eventStop";
+	
+	var p = TimeLine.prototype;
+	
+	/**
+	 * (private) function used to interpolate time with requestAnimationFrame
+	 */
+	p.ease = function() {
+		if (this.easing.on) {
+			var t = Date.now() - this.easing.startTime;
+			var d = this.easing.duration;
+			
+			if (t >= d) {
+				this.easing.on = false;
+				
+				this.setTime(this.easing.endValue);
+			}
+			else {
+				this.setTime(this.easing.easingFunction(t, this.easing.startValue, this.easing.endValue - this.easing.startValue, d));
+				
+				requestAnimationFrame(delegate(this.ease, this));
+			}
+		}
+	};
+	
+	/**
+	 * Move playhead to time
+	 * 
+	 * @param t : time 
+	 * @param options : 
+	 * - duration in milliseconds : default 1000
+	 * - easing : easing function : default easeOutQuad
+	 */
+	p.moveToTime = function(t, options) {
+		options = $.extend({
+			duration: Math.min(1000, Math.abs(t - this.time)),
+			easing : mf.utils.Maths.easeOutQuad
+		}, options);
+		
+		this.easing.startTime 		= Date.now();
+		this.easing.duration 		= options.duration;
+		this.easing.startValue 		= this.time;
+		this.easing.endValue 		= t;
+		this.easing.easingFunction 	= options.easing;
+		
+		if (!this.easing.on) {
+			this.easing.on = true;
+			requestAnimationFrame(delegate(this.ease, this));
+		}
+	};
+	
+	/**
+	 * Set the time and trigger the TimeLine.TIME_CHANGED event
+	 * 
+	 * @param t : time 
+	 */
+	p.setTime = function(t) {
+		if (t != this.time) {
+			this.time = t;
+			
+			$(this).trigger(TimeLine.TIME_CHANGED);
+		}
+	};
+	
+	/**
+	 * Start the playhead, linear time-based play
+	 */
+	p.play = function() {
+		$(this).trigger(TimeLine.PLAY);
+		
+		this.moveToTime(this.length, {
+			duration: this.length - this.time,
+			easing: mf.utils.Maths.easeLinear
+		});
+	};
+	
+	/**
+	 * Stop the playing
+	 */
+	p.stop = function(t) {
+		if (typeof t != "undefined") {
+			this.setTime(t);
+		}
+		
+		$(this).trigger(TimeLine.STOP);
+		
+		this.easing.on = false;
+	};
+})(mf = window.mf || {});
