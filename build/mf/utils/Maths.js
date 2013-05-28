e8474b1767bb516581811046e04b9474ac6252d6
zemax <maxime.cousinou@zemax.net>
2013-05-28 01:44:53 -0700

    Add 0 fix in distance calculation

--- build/mf/utils/Maths.js
+++ build/mf/utils/Maths.js
@@ -161,6 +161,10 @@
 		var i = (Math.cos(lat1) * Math.cos(lat2) * Math.cos(long1) * Math.cos(long2) + Math.cos(lat1) * Math.sin(long1) * Math.cos(lat2) * Math.sin(long2) + Math.sin(lat1) * Math.sin(lat2)); 
 		var j = (Math.acos(i));
 		
+		if (isNaN(j)) {
+			j = 0;
+		}
+		
 		return (radius * j);
 	};
 })(mf = window.mf || {});
